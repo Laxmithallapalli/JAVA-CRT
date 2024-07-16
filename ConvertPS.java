fd1f30ec25a631714b5556047cb00192615e0bf2
Laxmithallapalli <165120347+Laxmithallapalli@users.noreply.github.com>
2024-07-16 15:54:47 +0530

    ConvertPrimitiveStr.java

---
+++ ConvertPS.java
@@ -0,0 +1,12 @@
+class ConvertPS
+{
+public static void main(String args[])
+{
+int a=35721;
+String str=String.valueOf(a);
+for(int i=str.length()-1;i>=0;i--)
+{
+System.out.print(str.charAt(i));
+}
+}
+}
