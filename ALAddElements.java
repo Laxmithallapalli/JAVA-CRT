8b755c2a3b5be21b9233f10bb806654f08338be1
Laxmithallapalli <165120347+Laxmithallapalli@users.noreply.github.com>
2024-09-18 11:32:39 +0530

    ALAddElements.java

---
+++ ALAddElements.java
@@ -0,0 +1,17 @@
+//Add Elements to an ArrayList
+import java.util.*;
+class ALAddE
+{
+    public static void main(String args[])
+    {
+        ArrayList<String> languages=new ArrayList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("C");
+        System.out.println("ArrayList:"+languages);
+    }
+}
+/*
+*O/p:
+*ArrayList:[Java, Python, C]
+*/
