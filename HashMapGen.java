39e6d2515cd8464e063e56cd1425475eb916abde
Laxmithallapalli <165120347+Laxmithallapalli@users.noreply.github.com>
2024-09-19 10:57:03 +0530

    HashMapGen.java

---
+++ HashMapGen.java
@@ -0,0 +1,15 @@
+// Prgm Used for generic
+import java.util.*;
+public class HMG 
+{
+public static void main(String args[])
+{
+HashMap<Integer,String> h1=new HashMap<>();
+h1.put(101,"sss");
+h1.put(123,"kkk");
+h1.put(105,"bbb");
+System.out.println(h1);
+}
+}
+OUTPUT:
+{101=sss, 105=bbb, 123=kkk}
